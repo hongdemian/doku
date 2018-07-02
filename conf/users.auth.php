cbc0f9a28b2c7b071ff520c29b758406a5ea0d9f
Damien <hongdemian@gmail.com>
2018-07-01 20:35:17 -0600

    First Upload

---
+++ conf/users.auth.php
@@ -0,0 +1,14 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+hongdemian:$1$mzcTSLS8$nKTOs4B7cigvEYwX8yeWx/:Damien Hyatt:connectingreflections@gmail.com:admin,user
+vertigo:$1$veEwsNAH$WjXmOgDjGEUhTVvZcE0U..:For External Review:hongdemian@gmail.com:user
+jason:$1$q3GSnw3u$aZR61npyT.iQJ4Ymy57F81:Jason Laurie:jason@vertigoworkatheight.com:vertigo
---
+++ lib/tpl/SprintDocu/conf/metadata.php
@@ -0,0 +1,11 @@
+<?php
+
+$meta['sidebar_sections'] = array(
+                                'multichoice',
+                                '_choices' => array(
+                                    'h1',
+                                    'h1,h2',
+                                    'h1,h2,h3,h4,h5',
+                                    'li.level1 > .li',
+                                ),
+                            );
